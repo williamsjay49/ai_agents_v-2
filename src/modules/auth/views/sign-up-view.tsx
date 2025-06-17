3a7f80e3cfb7d2cdaf82c45a9bc318f3203c8a37
johnson williams <williams.gb55@gmail.com>
2025-06-17 15:23:16 +0900

    04:authentication-ui-2

--- src/modules/auth/views/sign-up-view.tsx
+++ src/modules/auth/views/sign-up-view.tsx
@@ -159,7 +159,7 @@ export const SignUpView = () => {
                   </Alert>
                 )}
                 <Button disabled={isLoading} type="submit" className="w-full">
-                  Sign in
+                  Sign up
                 </Button>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                   <span className="bg-card text-muted-foreground relative z-10 px-2">
